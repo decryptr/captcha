0d38e048990d101444318c1d3593b62a65fb0201
jtrecenti <julio.trecenti@gmail.com>
2021-12-27 10:50:25 -0300

    new decryptr fun

--- R/decrypt.R
+++ R/decrypt.R
@@ -5,17 +5,24 @@
 #'
 #' @export
 decrypt <- function(file, mm) {
-  vocab <- mm$parm$vocab
-  dims <- mm$parm$input_dim
-  mm$to(device = "cpu")
-  mm$eval()
-  ans <- calcular_x(file, dims)$unsqueeze(1) %>%
-    valid_transforms() %>%
-    mm() %>%
-    torch::torch_max(dim = 3) %>%
-    purrr::pluck(2)
-  paste(
-    vocab[as.numeric(ans$to(device = "cpu"))],
-    collapse = ""
-  )
+  # vocab <- mm$parm$vocab
+  # dims <- mm$parm$input_dim
+  # mm$to(device = "cpu")
+  # mm$eval()
+  # ans <- calcular_x(file, dims)$unsqueeze(1) %>%
+  #   valid_transforms() %>%
+  #   mm() %>%
+  #   torch::torch_max(dim = 3) %>%
+  #   purrr::pluck(2)
+  # paste(
+  #   vocab[as.numeric(ans$to(device = "cpu"))],
+  #   collapse = ""
+  # )
+  mm$model$eval()
+  transformed <- captcha_transform_image(file)$unsqueeze(2)
+  ind <- mm$model(transformed) |>
+    torch::torch_argmax(3) |>
+    as.matrix()
+  apply(ind, 1, \(x) paste(captcha_ds$vocab[x], collapse = ""))
 }
+
