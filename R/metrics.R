a3cc69a93464b5124c6f0a8587fedc9e8dc0fbf7
jtrecenti <julio.trecenti@gmail.com>
2021-12-28 20:51:54 -0300

    accuracy of full captcha not tokens

--- R/metrics.R
+++ R/metrics.R
@@ -12,9 +12,11 @@ captcha_accuracy <- luz::luz_metric(
     pred <- torch::torch_argmax(preds, dim = 3)
     tgt <- torch::torch_argmax(target, dim = 3)
     # browser()
-    new_correct <- (pred == tgt)$to(dtype = torch::torch_float())$sum()$item()
+
+    new_correct <- torch::torch_sum(pred == tgt, 2) == dim(pred)[2]
+    new_correct <- new_correct$to(dtype = torch::torch_float())$sum()$item()
     self$correct <- self$correct + new_correct
-    self$total <- self$total + pred$numel()
+    self$total <- self$total + dim(pred)[1]
   },
   compute = function() {
     self$correct / self$total
