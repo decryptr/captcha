a3e25614b3e12b478a0bd61d327dad95e83f4cc0
Julio Trecenti <julio.trecenti@gmail.com>
2023-02-21 18:12:57 -0300

    edit torch config on testthat

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,14 +1,13 @@
 # This file is part of the standard setup for testthat.
 # It is recommended that you do not modify it.
 #
-# Where should you do additional test configuration?
-# Learn more about the roles of various files in:
-# * https://r-pkgs.org/tests.html
-# * https://testthat.r-lib.org/reference/test_package.html#special-files
+# I changed it only to install torch when the user does not have it.
 
 library(testthat)
 library(captcha)
 
-torch::install_torch()
+if (!torch::torch_is_installed()) {
+  torch::install_torch()
+}
 
 test_check("captcha")
