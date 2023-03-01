e00a81700d7a0e3f1fb9b4984a173442d47ff8bd
Julio Trecenti <julio.trecenti@gmail.com>
2023-03-01 13:30:55 -0300

    change snapshot

--- tests/testthat/_snaps/utils.md
+++ tests/testthat/_snaps/utils.md
@@ -3,8 +3,6 @@
     Code
       print(captcha)
     Output
-      # A tibble: 1 x 7
         format width height colorspace matte filesize density
-        <chr>  <int>  <int> <chr>      <lgl>    <int> <chr>  
-      1 PNG      160     50 sRGB       TRUE      2462 72x72  
+      1    PNG   160     50       sRGB  TRUE     2462   72x72
 
--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,5 +1,4 @@
 test_that("print captcha as expected", {
-
   # skip if running in knitr context
   testthat::skip_if(isTRUE(getOption('knitr.in.progress')))
   f_captcha <- test_path("examples/tjpe.png")
@@ -8,17 +7,16 @@ test_that("print captcha as expected", {
 
 })
 
+# from the testthat documentation
+save_png <- function(code, width = 400, height = 400) {
+  path <- tempfile(fileext = ".png")
+  png(path, width = width, height = height)
+  on.exit(dev.off())
+  code
+  path
+}
 
 test_that("plot captcha as expected", {
-
-  # from the testthat documentation
-  save_png <- function(code, width = 400, height = 400) {
-    path <- tempfile(fileext = ".png")
-    png(path, width = width, height = height)
-    on.exit(dev.off())
-    code
-    path
-  }
 
   f_captcha <- test_path("examples/tjpe.png")
   captcha <- read_captcha(f_captcha)
@@ -28,19 +26,8 @@ test_that("plot captcha as expected", {
 
 test_that("plot captcha as expected, big images", {
 
-  # from the testthat documentation
-  save_png <- function(code, width = 400, height = 400) {
-    path <- tempfile(fileext = ".png")
-    png(path, width = width, height = height)
-    on.exit(dev.off())
-    code
-    path
-  }
-
   f_captcha <- rep(test_path("examples/tjpe.png"), 110)
-
   captcha <- read_captcha(f_captcha)
-
   expect_message(
     expect_snapshot_file(save_png(plot.captcha(captcha)), "plot_many.png"),
     "Too many"
@@ -50,18 +37,8 @@ test_that("plot captcha as expected, big images", {
 test_that("plot captcha with annotation", {
 
   if (check_magick_ghostscript(error = FALSE)) {
-    # from the testthat documentation
-    save_png <- function(code, width = 400, height = 400) {
-      path <- tempfile(fileext = ".png")
-      png(path, width = width, height = height)
-      on.exit(dev.off())
-      code
-      path
-    }
-
     f_captcha <- test_path("examples/tjpe_4wba3.png")
     captcha <- read_captcha(f_captcha)
-
     expect_snapshot_file(save_png(plot.captcha(captcha)), "plot_annotation.png")
   } else {
     expect_error(check_magick_ghostscript())
@@ -80,7 +57,4 @@ test_that("subset and length works", {
   expect_length(captcha_subs, 4)
   expect_equal(length(captcha), 10)
   expect_equal(length(captcha_subs), 4)
-
-
-
 })
